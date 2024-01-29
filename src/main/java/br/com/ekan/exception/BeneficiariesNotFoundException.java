913630945c971e3f6a3a55798f7304019ee5d4d8
Eduardo Firino <37375109+edufirino@users.noreply.github.com>
2024-01-29 17:31:00 -0300

    Update BeneficiariesNotFoundException.java

--- src/main/java/br/com/ekan/exception/BeneficiariesNotFoundException.java
+++ src/main/java/br/com/ekan/exception/BeneficiariesNotFoundException.java
@@ -3,7 +3,7 @@ package br.com.ekan.exception;
 public class BeneficiariesNotFoundException extends RuntimeException {
 
     public BeneficiariesNotFoundException() {
-        super("Beneficiarios não encontrado.");
+        super("Beneficiary not found.");
     }
 
 }
